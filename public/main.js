4db2c75dc095438015f0efca4381a6cca7b7e1b4
Victoria Tiller <vijotiller@gmail.com>
2021-05-19 15:03:35 -0400

    score min and max

--- public/main.js
+++ public/main.js
@@ -3,37 +3,57 @@ let score2 = 0
 
 function handleTeam1Click(event) {
   const thingClickedOn = event.target
+  if (score1 === 21 || score2 === 21) {
+    console.log('game is over')
+    return
+  }
 
   if (thingClickedOn.classList.contains('add')) {
     score1++
   }
   if (thingClickedOn.classList.contains('subtract')) {
+    if (score1 === 0) {
+      console.log("can't go negative")
+      return
+    }
     score1--
   }
-  const scoreHeader = document.querySelector('section.team1 h3')
+  const scoreHeader1 = document.querySelector('section.team1 h3')
 
-  scoreHeader.textContent = score1
+  scoreHeader1.textContent = score1
+
+  if (score1 >= 21) {
+    const header = document.querySelector('h1')
+    header.textContent = 'Team 1 wins!'
+  }
 }
 function handleTeam2Click(event) {
   const thingClickedOn = event.target
+  if (score2 === 21 || score1 === 21) {
+    console.log('game is over')
+    return
+  }
 
   if (thingClickedOn.classList.contains('add')) {
     score2++
   }
   if (thingClickedOn.classList.contains('subtract')) {
+    if (score2 === 0) {
+      console.log("can't go negative")
+      return
+    }
     score2--
   }
   const scoreHeader2 = document.querySelector('section.team2 h3')
 
   scoreHeader2.textContent = score2
+
+  if (score2 >= 21) {
+    const header = document.querySelector('h1')
+    header.textContent = 'Team 2 wins!'
+  }
 }
 function main() {
-  // const allButtons = document.querySelectorAll('i')
-
-  // allButtons.forEach(function (button) {
-  //   button.addEventListener('click', handleClickSquare)
-  // })
-
   const scoreBoard1 = document.querySelector('section.team1')
   scoreBoard1.addEventListener('click', handleTeam1Click)
 
