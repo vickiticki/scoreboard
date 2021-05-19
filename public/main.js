a209f6decca7e5328db8e71a9837c54c972c06d2
Victoria Tiller <vijotiller@gmail.com>
2021-05-19 14:13:39 -0400

    can change team 1 score

--- public/main.js
+++ public/main.js
@@ -1,7 +1,28 @@
-function main() {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+let score = 0
+
+function handleTeam1Click(event) {
+  const thingClickedOn = event.target
+
+  if (thingClickedOn.classList.contains('add')) {
+    score++
   }
+  if (thingClickedOn.classList.contains('subtract')) {
+    score--
+  }
+  const scoreHeader = document.querySelector('h3')
+
+  scoreHeader.textContent = score
+}
+
+function main() {
+  // const allButtons = document.querySelectorAll('i')
+
+  // allButtons.forEach(function (button) {
+  //   button.addEventListener('click', handleClickSquare)
+  // })
+
+  const scoreBoard = document.querySelector('section.team1')
+  scoreBoard.addEventListener('click', handleTeam1Click)
 }
 
 document.addEventListener('DOMContentLoaded', main)
